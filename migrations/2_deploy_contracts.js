8d45cdb8e646dbcb4dffa3cd2e95d4b6594742e5
Justin Hamilton <justinhamilton@Justins-MacBook-Pro.local>
2018-02-17 10:46:42 -0700

    Add initial solity contract interface for PutUpOrShutUp

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,6 @@
 var Adoption = artifacts.require("Adoption");
+var PutUpOrShutUp = artifacts.require("PutUpOrShutUp");
+var Bet = artifacts.require("Bet");
 
 module.exports = function(deployer) {
   deployer.deploy(Adoption);
