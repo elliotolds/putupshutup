ce60f793b7f2e11c3b54d3cdf7857e458e97f85a
Matthew G <matthewgard1@RedSled>
2018-02-17 12:43:43 -0700

    split App js for new bet

---
+++ src/js/app-new-bet.js
@@ -0,0 +1,69 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  ipfs: null,
+
+  init: function() {
+    App.ipfs = new IPFS();
+    App.ipfs.setProvider({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+    const hash = "QmQtwERJbXkS34fdQbk4HLyyW9sc4TXZ4XvAMUZwzCXPWD";
+    App.ipfs.cat(hash, (err, data) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("DATA:", data);
+    });
+
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Is there an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('Bet.json', function(data) {
+
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var BetArtifact = data;
+      App.contracts.Bet = TruffleContract(BetArtifact);
+    
+      // Set the provider for our contract
+      App.contracts.Bet.setProvider(App.web3Provider);
+    
+      // Use our contract to retrieve and mark the adopted pets
+      return loadBets();
+    });
+
+    return App.bindEvents();
+  },
+
+  loadBets: function() {
+    console.log("Hello world!"); 
+  },
+
+  bindEvents: function() {
+    $('#test-btn').click(App.betButton)
+  },
+
+  betButton: function() {
+    let a = new Bet(App.contracts, App.ipfs)
+    a.getFundingStatus()
+  },
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
