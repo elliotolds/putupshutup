36ca321c30b4847bb118447e88a404b93d688e75
Justin Hamilton <justinhamilton@Justins-MacBook-Pro.local>
2018-02-17 14:09:19 -0700

    Add getDemoBet()

--- contracts/PutUpOrShutUp.sol
+++ contracts/PutUpOrShutUp.sol
@@ -38,6 +38,21 @@ contract PutUpOrShutUp {
   function getBetsForUser(address _userAddress) public view returns (bytes32[]) {
     return betsForUser[_userAddress];
   }
+
+  function getDemoBet() public view returns (Bet) {
+
+    Bet b = new Bet(
+        0xf17f52151EbEF6C7334FAD080c5704D77216b732, 
+        10, 
+        0x627306090abaB3A6e1400e9345bC60c78a8BEf57, 
+        10, 
+        0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef,
+        2,
+        "fdksfhsdfdkasfjsdklaf"
+        );
+
+    return b;
+  }
 }
 
 contract Bet {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
 var Adoption = artifacts.require("Adoption");
 var PutUpOrShutUp = artifacts.require("PutUpOrShutUp");
-var Bet = artifacts.require("Bet");
 
 module.exports = function(deployer) {
   deployer.deploy(Adoption);
   deployer.deploy(PutUpOrShutUp);
-  deployer.deploy(Bet);
 };
